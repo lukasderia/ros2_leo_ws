92582813c8da9afab535a67168893a23ed959913
Lukas Duzakin <lukasduzakin@hotmail.com>
2025-12-12 15:05:19 +0100

    Small fixes to OdomTF script.

--- src/leo_utils/scripts/odom2TF.py
+++ src/leo_utils/scripts/odom2TF.py
@@ -5,6 +5,9 @@ from rclpy.node import Node # base class for all node
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
 from tf2_ros import TransformBroadcaster
+from tf_transformations import euler_from_quaternion, quaternion_from_euler
+from geometry_msgs.msg import Quaternion
+
 
 
 class odom2TF(Node):
@@ -18,17 +21,28 @@ class odom2TF(Node):
 
     def odom_callback(self, msg):
         t = TransformStamped()
-        # Copy headers and posse to re-broadcast them
-        t.header = msg.header
-        t.header.stamp = msg.header.stamp #self.get_clock().now().to_msg()
-        t.child_frame_id = msg.child_frame_id
+
+        t.header.frame_id = "odom"
+        t.child_frame_id = "base_link"
+        t.header.stamp = self.get_clock().now().to_msg()
         t.transform.translation.x = msg.pose.pose.position.x
         t.transform.translation.y = msg.pose.pose.position.y
-        t.transform.translation.z = msg.pose.pose.position.z
-        t.transform.rotation = msg.pose.pose.orientation
+        t.transform.translation.z = 0.0 # msg.pose.pose.position.z
+        q = msg.pose.pose.orientation
+        yaw = self.quat_to_yaw(q)          # extract yaw from quaternion
+        t.transform.rotation = self.yaw_to_quat(yaw)
 
         # Publish transform
         self.tf_broadcaster.sendTransform(t)
+
+    def quat_to_yaw(self, q):
+        # q is geometry_msgs.msg.Quaternion
+        roll, pitch, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+        return yaw
+    
+    def yaw_to_quat(self, yaw):
+        qx, qy, qz, qw = quaternion_from_euler(0.0, 0.0, yaw)
+        return Quaternion(x=qx, y=qy, z=qz, w=qw)
 
 
 def main(args=None):
