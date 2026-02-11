f9399032b9dff7e043b9e782a39e15c1be43e755
Lukas Duzakin <lukasduzakin@hotmail.com>
2026-02-11 13:18:42 +0100

    Small changes synced automatically

--- src/leo_bringup/launch/jetson_full_exp.launch.py
+++ src/leo_bringup/launch/jetson_full_exp.launch.py
@@ -12,6 +12,12 @@ from launch.actions import TimerAction
 # Full launcher with explroation
 def generate_launch_description():
 
+    # Include Teleop
+    teleop_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('leo_teleop'), 'launch', 'controller_teleop.launch.py')])
+    )
+
     # Include Robot Description
     description_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
@@ -60,6 +66,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        teleop_launch,
         description_launch,
         odom2TF_node,
         velodyne_launch,
